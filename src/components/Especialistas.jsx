939eddf651c677598b311cb6d6d71e990cdec232
Felipe Giacomini Cocco <felipegco@proton.me>
2026-01-18 16:05:40 -0300

    - Fixed typo in `EspecialistasCard` component name and updated its structure for better usability. - Enhanced styling for `Especialistas` component cards, including new image handling. - Added Vanessa's profile picture and updated dependencies with `pnpm`.

--- src/components/Especialistas.jsx
+++ src/components/Especialistas.jsx
@@ -1,23 +1,25 @@
 import './Especialistas.css'
 import EspecialistasCard from "./EspecialistasCard/EspecialistasCard.jsx";
 
+import FotoDraVanessa from "../assets/DraVanessa.png";
+
 function Especialistas() {
     return (
         <div id="especialistas">
             <h2>Conheça Nossas Especialistas</h2>
             <div className="especialistas_wrap">
                 <EspecialistasCard
-                    FotoDra="https://via.placeholder.com/300x400.png/a7a7a7/ffffff?"
+                    FotoDra=""
                     NomeDra="Dra A"
                     FuncaoDra="Clínica Geral e Estética"
                 />
                 <EspecialistasCard
-                    FotoDra="https://via.placeholder.com/300x400.png/a7a7a7/ffffff?"
-                    NomeDra="Dra B"
+                    FotoDra={FotoDraVanessa}
+                    NomeDra="Dra Vanessa"
                     FuncaoDra="Ortodontia"
                 />
                 <EspecialistasCard
-                    FotoDra="https://via.placeholder.com/300x400.png/a7a7a7/ffffff?"
+                    FotoDra=""
                     NomeDra="Dra C"
                     FuncaoDra="Implantodontia e Prótese"
                 />
--- src/components/EspecialistasCard/EspecialistasCard.jsx
+++ src/components/EspecialistasCard/EspecialistasCard.jsx
@@ -1,12 +1,13 @@
-function EspacialistasCard(props) {
+function EspecialistasCard(props) {
     return (
         <div className="especialistas_card">
-            <svg width="100" height="100" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <img src={props.FotoDra}
-                     alt="Foto da Dra. A"/>
-            </svg>
+            <img
+                src={props.FotoDra}
+                alt={`Foto da ${props.NomeDra}`}
+                className="foto-especialista"
+            />
             <h3>{props.NomeDra}</h3>
             <p>{props.FuncaoDra}</p>
         </div>
     )
-} export default EspacialistasCard;
+} export default EspecialistasCard;
